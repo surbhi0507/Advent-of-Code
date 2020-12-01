ebaf8db4e6f483c30768240cbb0064dd3b011bfe
surbhi0507 <43854235+surbhi0507@users.noreply.github.com>
2020-12-01 21:25:51 +0530

    Update Day1 q1.py

--- Day1 q1.py
+++ Day1 q1.py
@@ -1,3 +1,4 @@
+#Challenge-1
 li = [1721,979,366,299,675,1456]
 lis = []
 for i in li:
@@ -8,6 +9,20 @@ for i in li:
                 if ans not in lis:
                     print(i, ' x ', j, ' = ', ans)
                     lis.append(ans)
+
+                    
+#Challenge-2
+lis = []
+for i in li:
+    for j in li:
+        for k in li:
+            if i!=j and i!=k and j!=k:
+                if i+j+k == 2020:
+                    ans = i*j*k
+                    if ans not in lis:
+                        print(i, 'x', j, 'x', k, ' = ', ans)
+                        lis.append(ans)
                 
+
 
 
